03121fc45d76c25cc283b1764df6245f4d49cddf
ardianrivaulhak <ardianrivaulhak@gmail.com>
2026-08-24 21:52:13 +0700

    dsadsa

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -48,10 +48,9 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF1C1C1E), Color(0xFF2A2A2D)],
+          image: DecorationImage(
+            image: AssetImage('assets/images/background1.jpg'),
+            fit: BoxFit.cover,
           ),
         ),
         child: SafeArea(
@@ -73,10 +72,12 @@ class _LoginScreenState extends State<LoginScreen> {
                       width: 100,
                       height: 100,
                       decoration: BoxDecoration(
-                        color: const Color(0xFF1565C0),
                         borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/background2.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: const Icon(Icons.sports_motorsports, size: 60, color: Colors.white),
                     ),
                     const SizedBox(height: 24),
                     const Text('Helmet Store & Custom', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white)),
